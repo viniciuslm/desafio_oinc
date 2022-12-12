623e60ea435ba04789b79eeab8f5147a70799c24
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-12-12 11:07:29 -0300

    remove page controller test

--- test/oinc_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule OincWeb.PageControllerTest do
-  use OincWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
